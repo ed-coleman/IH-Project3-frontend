c8699e418dc73288d401ed67028be8c81c09d11a
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-07 11:08:25 +0100

    tested some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import HomePage from './Pages/HomePage'
 import ProductPage from './Pages/ProductPage'
 import SellProductPage from './Pages/SellProductPage'
 import UpdatePage from './Pages/UpdatePage'
+import ReviewPage from './Pages/ReviewPage'
+import DetailedReviewPage from './Pages/DetailedReviewPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +19,10 @@ function App() {
      <Route path='/products/:productId' element={<ProductPage/>} />
      <Route path='/products/new' element={<SellProductPage/>} />
      <Route path='/products/update/:productId' element={<UpdatePage/>} />
-      <Route path='x' element={<h1>404 Not Found</h1>} />
+     <Route path='/reviews' element={<ReviewPage/>} />
+     <Route path='/reviews/:reviewId' element={<DetailedReviewPage/>} />
+
+     <Route path='x' element={<h1>404 Not Found</h1>} />
      </Routes>
     </div>
   )
