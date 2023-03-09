f1da60a8264086a23244b0941470cc75ebe92122
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-09 15:29:34 +0100

    changed div height to auto

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -23,9 +23,9 @@ export default function HomePage() {
     <ul style={{listStyleType: "none", display: "flex", flexWrap: "wrap"}}>
       {products.map(product => (
         <li key={product._id} style={{flex: "0 0 33.333333%"}}>
-          <div style={{width: "250px", height: "300px", border: "3px solid black", padding: "10px", margin: "10px"}}>
+          <div style={{width: "300px", height: "auto", border: "3px solid black", padding: "10px", margin: "10px"}}>
           <Link to={`/products/${product._id}`}><p style={{color: "black"}}>{product.title}</p>
-          <img style={{width: "290", height: "auto", color: "black"}} src={product.image} alt="this is a pic"/>
+          <img style={{width: "90%", height: "auto", color: "black"}} src={product.image} alt="this is a pic"/>
           <p style={{color: "black"}}>€{product.price}</p>
           </Link>
           </div>
